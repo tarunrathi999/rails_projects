6a4be60ea9660df982d0ebde18bfc8052fa2f192
tarun rathi <tarunrathi999@gmail.com>
2015-09-02 12:21:08 +0530

    first commit

---
+++ movie_review/app/models/movie.rb
@@ -0,0 +1,6 @@
+class Movie < ActiveRecord::Base
+	belongs_to :user
+	has_many :reviews
+	 has_attached_file :image, styles: { medium: "400x600",thumb: "100x100" }
+ 	 validates_attachment_content_type :image,content_type:  /\Aimage\/.*\Z/
+end
